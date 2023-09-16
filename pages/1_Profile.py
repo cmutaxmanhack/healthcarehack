06401f607aa55eb65669c360a4406b635c3b017c
cmutaxmanhack <141285170+cmutaxmanhack@users.noreply.github.com>
2023-09-16 19:49:29 -0400

    Update 1_Profile.py

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -3,4 +3,4 @@ from PIL import Image
 
 st.header('***My Profile***')
 image = Image.open('profilepicmichelle.jfif')
-    st.image(image, width= None, use_column_width=True)
+st.image(image, width= None, use_column_width=True)
