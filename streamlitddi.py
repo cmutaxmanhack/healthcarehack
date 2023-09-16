b7e70fa7e4ff2ff40ddbb7ef2c84cdc648c6a6a6
cmutaxmanhack <141285170+cmutaxmanhack@users.noreply.github.com>
2023-09-16 17:13:13 -0400

    Update streamlitddi.py

--- streamlitddi.py
+++ streamlitddi.py
@@ -1,10 +1,28 @@
-import numpy
 import pandas as pd
 import streamlit as st
 
+# Function to get drug interactions
+def get_drug_interactions(df, drug_name):
+    interactions = df[df.iloc[:, 0] == drug_name].iloc[:, 1].tolist()
+    return interactions
 
-DataDf = pd.read_csv("response_to_antidepressant.tsv", sep="\t")
+# Main Streamlit application
+def main():
+    DataDf = pd.read_csv("response_to_antidepressant.tsv", sep="\t")
+    # Reading drug interactions data
+    ddi_df = pd.read_csv("ddi.csv")
 
+    unique_drugs = ddi_df.iloc[:, 0].unique()
+    selected_drug = st.selectbox("Select a drug to check its interactions:", unique_drugs)
+    
+    # Show drug interactions
+    interactions = get_drug_interactions(ddi_df, selected_drug)
+    if len(interactions) == 0:
+        st.write(f"No known interactions for {selected_drug}.")
+    else:
+        st.write(f"The following drugs interact with {selected_drug}:")
+        for interaction in interactions:
+            st.write(f"- {interaction}")
 
 #TestDf = pd.read_csv("patient3.tsv", sep="\t")
 
