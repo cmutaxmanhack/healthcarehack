b35a02f577be058a705c61e16c1f94671c1e1847
cmutaxmanhack <141285170+cmutaxmanhack@users.noreply.github.com>
2023-09-16 19:51:34 -0400

    Update 1_Profile.py

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -2,5 +2,5 @@ import streamlit as st
 from PIL import Image
 
 st.header('***My Profile***')
-image = Image.open('profilepicmichelle.jfif')
+image = Image.open('profilepicmichelle.png')
 st.image(image, width= None, use_column_width=True)
