c628d1166c92f56512db162610c14bb0bb0a182d
cmutaxmanhack <141285170+cmutaxmanhack@users.noreply.github.com>
2023-09-16 19:58:56 -0400

    Update 1_Profile.py

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -2,5 +2,8 @@ import streamlit as st
 from PIL import Image
 
 st.header('***My Profile***')
+
+st.markdown ("Name")
+st.markdown ("Age")
 image = Image.open('profilepicmichelle.png')
 st.image(image, width= None, use_column_width=True)
