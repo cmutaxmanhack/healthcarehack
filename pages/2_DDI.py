2930637b1207e26d39f54be8a99a012097a24cda
cmutaxmanhack <141285170+cmutaxmanhack@users.noreply.github.com>
2023-09-16 18:59:23 -0400

    Update 2_DDI.py

--- pages/2_DDI.py
+++ pages/2_DDI.py
@@ -1,14 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-
-DataDf = pd.read_csv("response_to_antidepressant.tsv", sep="\t")
-
-option = st.selectbox(
-        'What medication are you looking for?',
-        ('Imipramine', 'Amitriptyline', 'Doexpin', 'Desipramine', 'Isocarboxazid', 'Fluoxetine', 'Venlafaxine', 'Bupropion'))
-st.write('You selected:', option)
-
 # Function to get drug interactions
 def get_drug_interactions(df, drug_name):
     interactions = df[df.iloc[:, 0] == drug_name].iloc[:, 1].tolist()
@@ -18,22 +10,26 @@ def get_drug_interactions(df, drug_name):
 def main():
     # Your existing code for antidepressants
     DataDf = pd.read_csv("response_to_antidepressant.tsv", sep="\t")
+    
     # Reading drug interactions data
     ddi_df = pd.read_csv("ddi.csv")
 
-    # Streamlit dropdown for drug interactions
-    
-    
-    #st.subheader("Check Drug Interactions")
-    #unique_drugs = ddi_df.iloc[:, 0].unique()
-    #selected_drug = st.selectbox("Select a drug to check its interactions:", unique_drugs)
+    # Streamlit dropdown for drug selection
+    option = st.selectbox(
+        'What medication are you looking for?',
+        ('Imipramine', 'Amitriptyline', 'Doexpin', 'Desipramine', 'Isocarboxazid', 'Fluoxetine', 'Venlafaxine', 'Bupropion')
+    )
+    st.write('You selected:', option)
     
     # Show drug interactions
-    interactions = get_drug_interactions(ddi_df, selected_drug)
+    interactions = get_drug_interactions(ddi_df, option)
     if len(interactions) == 0:
-        st.write(f"No known interactions for {selected_drug}.")
+        st.write(f"No known interactions for {option}.")
     else:
-        st.write(f"The following drugs interact with {selected_drug}:")
+        st.write(f"The following drugs interact with {option}:")
         for interaction in interactions:
             st.write(f"- {interaction}")
 
+# Run the main function to execute the Streamlit app
+if __name__ == '__main__':
+    main()
