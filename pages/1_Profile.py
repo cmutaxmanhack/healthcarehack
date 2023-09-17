559b7749d1051c150b2b4680f27bdf45264fa0a5
cmutaxmanhack <141285170+cmutaxmanhack@users.noreply.github.com>
2023-09-16 20:12:27 -0400

    Update 1_Profile.py

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -1,31 +1,58 @@
 import streamlit as st
 from PIL import Image
 
+# Initialize session state
+if 'profile_saved' not in st.session_state:
+    st.session_state['profile_saved'] = False
+
 # Title
 st.header('***My Profile***')
 
-# User Information
-st.subheader('Personal Information')
-name = st.text_input("Name:", "")
-age = st.number_input("Age:", min_value=0, max_value=120, step=1)
-sex = st.selectbox("Sex:", ["Male", "Female", "Other"])
+if not st.session_state['profile_saved']:
+    # Profile Image
+    image = Image.open('profilepicmichelle.png')
+    st.image(image, caption='Profile Picture', use_column_width=True)
 
-# Physical Details
-st.subheader('Physical Details')
-height = st.number_input("Height (cm):", min_value=0.0, max_value=300.0, step=0.1)
-weight = st.number_input("Weight (kg):", min_value=0.0, max_value=300.0, step=0.1)
+    # Personal Information
+    st.subheader('Personal Information')
+    name = st.text_input("Name:", "")
+    age = st.number_input("Age:", min_value=0, max_value=120, step=1)
+    sex = st.selectbox("Sex:", ["Male", "Female", "Other"])
 
-# Health Information
-st.subheader('Health Information')
+    # Physical Details
+    st.subheader('Physical Details')
+    height = st.number_input("Height (cm):", min_value=0.0, max_value=300.0, step=0.1)
+    weight = st.number_input("Weight (kg):", min_value=0.0, max_value=300.0, step=0.1)
 
-# Medications
-medications = st.text_area("Medications:", "None")
+    # Health Information
+    st.subheader('Health Information')
 
-# Known Conditions
-conditions = st.text_area("Known Conditions:", "None")
+    # Medications
+    medications = st.text_area("Medications:", "None")
 
-# When user is done, they can press the button to save their profile
-if st.button("Save Profile"):
+    # Known Conditions
+    conditions = st.text_area("Known Conditions:", "None")
+
+    # Save Profile Button
+    if st.button("Save Profile"):
+        st.session_state['profile_saved'] = True
+        st.session_state['name'] = name
+        st.session_state['age'] = age
+        st.session_state['sex'] = sex
+        st.session_state['height'] = height
+        st.session_state['weight'] = weight
+        st.session_state['medications'] = medications
+        st.session_state['conditions'] = conditions
+        st.experimental_rerun()
+else:
+    # Profile Information Display
+    st.subheader(f'Name: {st.session_state["name"]}')
+    st.subheader(f'Age: {st.session_state["age"]}')
+    st.subheader(f'Sex: {st.session_state["sex"]}')
+    st.subheader(f'Height: {st.session_state["height"]} cm')
+    st.subheader(f'Weight: {st.session_state["weight"]} kg')
+    st.subheader(f'Medications: {st.session_state["medications"]}')
+    st.subheader(f'Known Conditions: {st.session_state["conditions"]}')
     st.success("Profile Saved Successfully")
 
-# Here you can add code to save these details to a database, a file, or any other backend storage.
+    # Here you can add code to save these details to a database, a file, or any other backend storage.
