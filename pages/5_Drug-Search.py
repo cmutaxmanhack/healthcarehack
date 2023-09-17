e01f34064e15bd72a372c865833db6a994952324
sruthimalineni <125047319+sruthimalineni@users.noreply.github.com>
2023-09-16 23:56:56 -0400

    Update 5_Drug-Search.py

--- pages/5_Drug-Search.py
+++ pages/5_Drug-Search.py
@@ -4,18 +4,12 @@ from PIL import Image
 
 st.set_page_config(
     page_title="DDI",
-    page_icon="💊",
+    page_icon="🔍",
 )
 
 
-st.title("Drug-Drug Interaction")
+st.title("Drug Search")
 
-multi = '''A change in a drug’s effect on the body when the drug is taken together with a 
-        second drug. A drug-drug interaction can delay, decrease, or enhance the 
-        absorption of either drug. This can decrease or increase the action of either 
-        or both drugs or cause adverse effects.'''
-
-st.markdown(multi)
 
 with st.sidebar.container():
     image = Image.open('MUST_Icon.png')
@@ -25,7 +19,7 @@ DrugDf = pd.read_csv("Antidepressant.tsv", sep="\t")
 
 option = st.selectbox(
         'What medication are you looking for?',
-        ('Imipramine', 'Amitriptyline', 'Doexpin', 'Desipramine', 'Isocarboxazid', 'Citalopram', 'Venlafaxine', 'Bupropion', 'Buspirone', 'Sertraline', 'Escitalopram', 'Mirtazapine')
+        ('Imipramine', 'Amitriptyline', 'Doxepin', 'Desipramine', 'Isocarboxazid', 'Citalopram', 'Venlafaxine', 'Bupropion', 'Buspirone', 'Sertraline', 'Escitalopram', 'Mirtazapine')
     )
 st.write('You selected:', option)
 
