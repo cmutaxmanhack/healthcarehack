a5e7c065dd9157226047ad17d528dba0a6bd8d58
cmutaxmanhack <141285170+cmutaxmanhack@users.noreply.github.com>
2023-09-16 20:09:48 -0400

    Update 1_Profile.py

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -1,9 +1,31 @@
 import streamlit as st
 from PIL import Image
 
+# Title
 st.header('***My Profile***')
 
-st.markdown ("Name")
-st.markdown ("Age")
-image = Image.open('profilepicmichelle.png')
-st.image(image, width= None, use_column_width=True)
+# User Information
+st.subheader('Personal Information')
+name = st.text_input("Name:", "")
+age = st.number_input("Age:", min_value=0, max_value=120, step=1)
+sex = st.selectbox("Sex:", ["Male", "Female", "Other"])
+
+# Physical Details
+st.subheader('Physical Details')
+height = st.number_input("Height (cm):", min_value=0.0, max_value=300.0, step=0.1)
+weight = st.number_input("Weight (kg):", min_value=0.0, max_value=300.0, step=0.1)
+
+# Health Information
+st.subheader('Health Information')
+
+# Medications
+medications = st.text_area("Medications:", "None")
+
+# Known Conditions
+conditions = st.text_area("Known Conditions:", "None")
+
+# When user is done, they can press the button to save their profile
+if st.button("Save Profile"):
+    st.success("Profile Saved Successfully")
+
+# Here you can add code to save these details to a database, a file, or any other backend storage.
